c0f4a8468decf562e9ac2f1a7732b4795a1db21d
Jun <amares9100@gamil.com>
2023-02-01 17:53:42 +0900

    02/01 17:52 문의사항게시판/글쓰기 html 작성

---
+++ member/public/js/board.js
@@ -0,0 +1,31 @@
+
+let contents=[
+	{no : 1 , title : '샘플' , name : '재석' , ymd : '20230131' , read : 555},
+	{no : 2 , title : '샘플2' , name : '호동' , ymd : '20230131' , read : 555},
+	{no : 3 , title : '샘플3' , name : '석진' , ymd : '20230131' , read : 555}
+]
+
+
+content_print();
+
+function content_print(){
+	let html =`<tr>
+					<th width="10%">순번</th>
+					<th width="50%">제목</th>
+					<th width="10%">이름</th>
+					<th width="20%">등록일</th>
+					<th width="10%">조회수</th>
+				</tr>`;
+				
+	contents.forEach((o , i)=> {
+		
+		html += `<tr>
+					<td>${o.no}</td>
+					<td>${o.title}</td>
+					<td>${o.name}</td>
+					<td>${o.ymd}</td>
+					<td>${o.read}</td>
+				</tr>`
+	})
+	document.querySelector('.table').innerHTML=html;
+}
