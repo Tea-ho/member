0bc873f600ce33d54eb231110ebc3ae29d1fc2f4
504 <504@DESKTOP-TVIPHFD>
2023-02-07 15:23:12 +0900

    2/7 장바구니 수정 오후3:23

--- member/public/js/shopbasket.js
+++ member/public/js/shopbasket.js
@@ -12,7 +12,7 @@ cart_print();
 function cart_print(){
 	
 	let total = 0;
-	for( let j = 0 ; j<productList.length ; j++ ){ total += productList[j].price * productList[j].discount } 
+	for( let j = 0 ; j<productList.length ; j++ ){ total +=(productList[j].price-(productList[j].price * productList[j].discount)) } 
 	document.querySelector('.total').innerHTML = total.toLocaleString()+'원';
 	
 	let html=''
@@ -20,14 +20,14 @@ function cart_print(){
 	for(let i=0; i<productList.length; i++){                                    
 		
 		html+=`<tr class="박스">
-					<td widtd=5%><input class="ckck"  onclick="sel_box()"type="checkbox" checked="checked"></td> 
+					<td widtd=5%><input class="ckck"  onclick="sel_box(${i})"type="checkbox" checked="checked"></td> 
 					<td widtd=40%><img src="../img/${productList[i].img}" width="20%" height="20%"> </td>
 					 <td widtd=30% class="item_t">${productList[i].title}</td>
 					 <td widtd=5% class="item_s">${productList[i].size}</td>
 					 
 					 <td widtd=10%><span  class="item_p">${productList[i].price.toLocaleString()} 원 </span>
-					 <div widtd=10% class="item_disp">${(productList[i].price * productList[i].discount).toLocaleString()} 원 </div></td> 
-					 
+					 <div widtd=10% class="item_disp">${(productList[i].price-(productList[i].price * productList[i].discount)).toLocaleString()} 원 </div></td> 
+					
 					
 					 <td widtd=10%><button class="X"onclick="onDelete(${i})"type="button"> <img src="../img/삭제버튼.png" width="20%" height="20%"></button></td>
 					
@@ -82,5 +82,5 @@ function all_input(){
 //개별박스
 let s_box=true;
 function sel_box(i){
-
+console.log('cpcpcp')
 }
