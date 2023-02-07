2415f80636f6cbfcbb78440c5f618fb776fed51d
504 <504@DESKTOP-N3I93MK>
2023-02-07 14:02:27 +0900

    로그인 회원가입 마이페이지 푸터 추가, 마이페이지 수정

--- member/public/js/mypage.js
+++ member/public/js/mypage.js
@@ -1,3 +1,19 @@
-/**
- * 
- */
+
+function  out(){ //탈퇴유효성
+	console.log('작동')
+	
+	let id= prompt('아이디를 입력하세요')
+	let pw= prompt('비밀번호를 입력하세요')
+	
+	회원명단.forEach( (회원,i)=>{
+		if(id==회원.id&&pw==회원.pw){
+			let 재확인=prompt('회원탈퇴를 기입해주세요')
+			if(재확인=='회원탈퇴'){
+				alert('회원탈퇴되었습니다.')
+				회원명단.splice(i,1)
+				console.log(회원명단)
+			}
+			location.href='../index.html'
+		}else {alert( '아이디와 비밀번호를 확인하여 주세요')}
+	} )
+}
